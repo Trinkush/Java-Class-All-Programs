8bbb8ae6125b375a14b5b63b9e5876c977a66ba8
Trinkush <trinkushsingla23@gmail.com>
2018-02-18 17:29:19 +0530

    Changes

--- Mp3 Player In java/Player/src/playerMp3/PlayerMp3.java
+++ Mp3 Player In java/Player/src/playerMp3/PlayerMp3.java
@@ -1,15 +1,8 @@
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package playerMp3;
-//import sql.Database;
-//import add.Addition;
+
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-//import java.util.Date;
 import javazoom.jlme.util.Player;
 
 class Songs{
@@ -30,14 +23,8 @@ class Songs{
 public class PlayerMp3 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
         Songs obj = new Songs();
         obj.songplay("F:/So High.mp3");
-           //Sql obj1 = new Sql();
-           //sql obj1 = new sql();
-           //System.out.println(obj1.login("jdbc:mysql://localhost:3306/bank","root","pb11ay4108"));
-            //System.out.println(obj1.add(33 , 33));
-            //System.out.println(new Date().getTime());
 
     }
 
